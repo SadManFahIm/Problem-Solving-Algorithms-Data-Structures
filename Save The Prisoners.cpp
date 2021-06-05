48539fc7aafd3f5b50533bd6bfe01f0d3176e304
Sadman Fahim <57841708+SadManFahIm@users.noreply.github.com>
2021-06-06 00:11:03 +0600

    Commit

--- Save The Prisoners.cpp
+++ Save The Prisoners.cpp
@@ -1,21 +1,19 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-
-
+/*
+    *
+    * Prosen Ghosh
+    * American International University - Bangladesh (AIUB)
+    *
+*/
 int main() {
-    int t; cin >> t;
-    
-    while(t--) {
-        int n,m,s; cin >> n >> m >> s;
-        --s; --m;
-        s += m;
-        s %= n;
-        s++;
-        cout << s << endl;
+ 
+    int N,M,S,T;
+    cin >> T;
+    for(int t = 0; t < T; t++){
+        cin >> N >> M >> S;
+        if(!((M+S-1)%N))cout << N << endl;
+        else cout << (M+S-1)%N << endl;
     }
     return 0;
 }
